7b5815856a4dfd2db1e13cf2d64bdbe19d439dc3
Jason Best <bes22003@byui.edu>
2023-09-26 20:50:09 -0600

    finished learning activity3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        //Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimal());
+
+        Fraction fraction2 = new Fraction(27);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimal());
+
+        Fraction fraction3 = new Fraction(4, 34);
+        Console.WriteLine(fraction3.GetBottom());
+        Console.WriteLine(fraction3.GetTop());
+        Console.WriteLine(fraction3.GetDecimal());
+        Console.WriteLine(fraction3.GetFractionString());
+
+        
+
+
+        
     }
 }
