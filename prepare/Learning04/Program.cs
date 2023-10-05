7949e0da27d84d6610a3df992a159e13298fa40f
Jason Best <bes22003@byui.edu>
2023-10-05 11:50:00 -0600

    Completed Week 4 Learning

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        //Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("William Henderson", "math");
+        Console.WriteLine(a1.GetSummary());
+
+        Mathassignment a2 = new Mathassignment("Anthony Reber", "Multiplication", "2.3", "9-56");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomewordList());
+
+        WritingAssignment a3 = new WritingAssignment("Alicia Franklyn", "English", "How I became a 30 foot long pencil");
+        Console.WriteLine(a3.GetWritingInformation());
+        
+
     }
 }
